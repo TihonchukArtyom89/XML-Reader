99b7e005cc40ba31366a318cb732e9e63bf40af0
Тихончук Артём <artyom_tihonchuk_work@protonmail.com>
2022-05-06 18:17:51 +0300

    add Worker class and load xml file in listbox

--- XML-Reader/Form1.cs
+++ XML-Reader/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace XML_Reader
 {
@@ -15,6 +16,20 @@ namespace XML_Reader
         public Form1()
         {
             InitializeComponent();
+            ReceiveWorkers();
+        }
+        private void ReceiveWorkers()
+        {
+            XmlDocument xml_doc = new XmlDocument();
+            string xml_path = "F:\\Downloads\\MEGA\\XMLFile-source.xml";
+            xml_doc.Load(xml_path);
+            foreach(XmlNode node in xml_doc.DocumentElement)
+            {
+                string name = node.Attributes[0].Value;
+                int age = int.Parse(node["Age"].InnerText);
+                bool programmer = bool.Parse(node["Programmer"].InnerText);
+                NameWorker.Items.Add(new Worker(name,age,programmer));
+            }
         }
     }
 }
---
+++ XML-Reader/Worker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XML_Reader
+{
+    class Worker
+    {
+        public string Name { get; private set; }
+        public int  Age { get; private set; }
+        public bool Programmer { get; private set; }
+        public Worker(string name, int age, bool programmer)
+        {
+            Name = name;
+            Age = age;
+            Programmer = programmer;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
